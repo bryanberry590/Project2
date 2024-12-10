33d64b120336e42e6b2cefe38881583d48bd04f6
Spiurge <sergonzalez@csumb.edu>
2024-12-10 12:37:56 -0800

    Added admin activities

---
+++ app/src/main/java/com/example/project2/selectCreatureBuddyActivity.java
@@ -0,0 +1,12 @@
+package com.example.project2;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class selectCreatureBuddyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_creature_buddy);
+    }
+}
